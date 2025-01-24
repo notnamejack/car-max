33cdf48fd98be3549d90116580453258234e395f
Konstantinov Aleksey <KonstantinovAI@enplus.digital>
2025-01-24 11:09:21 +0800

    паралакс на телефоне

--- src/component/phone-section/phone-section.tsx
+++ src/component/phone-section/phone-section.tsx
@@ -26,82 +26,78 @@ export function PhoneSection({ scroll }: ISection) {
 	useEffect(() => {
 		if (scroll <= 1240) {
 			setActiveNav(0);
-			// if(ref.current)
-			// 	ref.current.style.height = `1191px`
-		}
-		if (scroll >= 1240) {
-			setActiveNav(1);
-			// if(ref.current)
-			// 	ref.current.style.height = `1191px`
 		}
 		if (scroll >= 1300) {
-			setActiveNav(2);
-			// if(ref.current)
-			// 	ref.current.style.height = `1231px`
+			setActiveNav(1);
 		}
-		if (scroll >= 1360) {
+		if (scroll >= 1500) {
+			setActiveNav(2);
+		}
+		if (scroll >= 1700) {
 			setActiveNav(3);
-			// if(ref.current)
-			// 	ref.current.style.height = `1271px`
 		}
 	}, [scroll]);
 
 	return (
 		<div className={s.container} ref={ref}>
-			<img src={image} className={s.backtound} />
-			<div className={s.body}>
-				<h2>REVOLUTION OF TELEGRAM GAMES</h2>
-				<ul>
-					{navItems.map((item, index) => (
-						<li key={`nav_${index}`}>
-							<button
-								className={`${index == activeNav && s.active}`}
-								onClick={() => setActiveNav(index)}>
-								{item}
-							</button>
-						</li>
-					))}
-				</ul>
-			</div>
-			<div className={s.container_phone}>
-				<div className={s.phone}>
-					<div className={s.phone_blur}></div>
-					<div className={s.pattern_items}>
-						<div className={s.cascade}>
-							<div className={s.cascade_main}>
-								<img src={one} />
-							</div>
-							<div className={s.cascade_doublee}>
-								<div className={clsx(s.cascade, activeNav > 0 && s.active)}>
-									<div className={s.cascade_main}>
-										<img src={two} />
-									</div>
-									<div className={s.cascade_doublee}>
-										<div className={clsx(s.cascade, activeNav > 1 && s.active)}>
-											<div className={s.cascade_main}>
-												<img src={tree} />
-											</div>
-											<div className={s.cascade_doublee}>
-												<div
-													className={clsx(
-														s.cascade,
-														activeNav > 2 && s.active
-													)}>
-													<div className={s.cascade_main}>
-														<img src={four} />
+			<div className={s.container_transform}>
+				<img src={image} className={s.backtound} />
+				<div className={s.body}>
+					<h2>REVOLUTION OF TELEGRAM GAMES</h2>
+					<ul>
+						{navItems.map((item, index) => (
+							<li key={`nav_${index}`}>
+								<button
+									className={`${index == activeNav && s.active}`}
+									onClick={() => setActiveNav(index)}>
+									{item}
+								</button>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className={s.container_phone}>
+					<div className={s.phone}>
+						<div className={s.phone_blur}></div>
+						<div className={s.pattern_items}>
+							<div className={s.cascade}>
+								<div className={s.cascade_main}>
+									<img src={one} />
+								</div>
+								<div className={s.cascade_doublee}>
+									<div className={clsx(s.cascade, activeNav > 0 && s.active)}>
+										<div className={s.cascade_main}>
+											<img src={two} />
+										</div>
+										<div className={s.cascade_doublee}>
+											<div
+												className={clsx(s.cascade, activeNav > 1 && s.active)}>
+												<div className={s.cascade_main}>
+													<img src={tree} />
+												</div>
+												<div className={s.cascade_doublee}>
+													<div
+														className={clsx(
+															s.cascade,
+															activeNav > 2 && s.active
+														)}>
+														<div className={s.cascade_main}>
+															<img src={four} />
+														</div>
+														<div className={s.cascade_doublee}></div>
 													</div>
-													<div className={s.cascade_doublee}></div>
 												</div>
 											</div>
 										</div>
 									</div>
 								</div>
 							</div>
+							<img src={phoneImg} className={s.pattern_phone} />
 						</div>
-						<img src={phoneImg} className={s.pattern_phone} />
 					</div>
 				</div>
 			</div>
+			<div className={s.contain}></div>
 		</div>
 	);
 }
